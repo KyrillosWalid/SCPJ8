ab691c20f79c94155fd9e363cdab19e7a3f456f6
Kyrillos Walid Radi <30301905+KOKADev@users.noreply.github.com>
2018-05-07 03:33:47 +0200

    Add files via upload

---
+++ A. Watermelon/S4AP.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class S4AP {
+    public static void main(String[] args) {
+        /*
+        **********************************************************
+        * THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
+        * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+        * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+        * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+        * WWW.FACEBOOK.COM/KOKADEV                               *
+        * WWW.GITHUB.COM/KOKADev                                 *
+        **********************************************************
+        */
+        Scanner input=new Scanner(System.in);
+        int w=input.nextInt();
+        if(w%2==0 && w>2){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+    
+}
