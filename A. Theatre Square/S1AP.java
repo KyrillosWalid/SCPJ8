cf968ab2bdf7389d4ea991f76b6b6148f354ab76
Kyrillos Walid Radi <30301905+KOKADev@users.noreply.github.com>
2018-05-07 23:50:37 +0200

    Create S1AP.java

---
+++ A. Theatre Square/S1AP.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class S1AP {
+    public static void main(String[] args) {
+        /*
+        **********************************************************
+        * THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
+        * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+        * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+        * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+        * WWW.FACEBOOK.COM/KOKADEV                               *
+        * WWW.GITHUB.COM/KOKADev                                 *
+        **********************************************************
+         */
+        Scanner input = new Scanner(System.in);
+        long m = input.nextLong();
+        long n = input.nextLong();
+        long a = input.nextLong();
+        long x = m / a;
+        long y = n / a;
+        if (m % a != 0) {
+            x = x + 1;
+        }
+        if (n % a != 0) {
+            y = y + 1;
+        }
+        long fs = x * y;
+        System.out.print(fs);
+
+    }
+
+}
