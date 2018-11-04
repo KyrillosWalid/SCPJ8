f1a666c1820fff9638b3274ffa0b064fe97507f0
Kyrillos Walid Radi <30301905+KyrillosWalid@users.noreply.github.com>
2018-11-04 18:41:09 +0200

    Update S4AP.java

--- A. Watermelon/S4AP.java
+++ A. Watermelon/S4AP.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class S4AP {
     public static void main(String[] args) {
-        /*
-        **********************************************************
-        * THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
-        * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
-        * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
-        * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
-        * WWW.FACEBOOK.COM/KOKADEV                               *
-        * WWW.GITHUB.COM/KOKADev                                 *
-        **********************************************************
-        */
+    /*
+   **********************************************************
+   * THIS CODE BY KYRILLOSWALIDRADI                         *
+   * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+   * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+   * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+   * WWW.FACEBOOK.COM/KyrillosWalid                         *
+   * WWW.GITHUB.COM/KyrillosWalid                           *
+   **********************************************************
+   */
         Scanner input=new Scanner(System.in);
         int w=input.nextInt();
         if(w%2==0 && w>2){
