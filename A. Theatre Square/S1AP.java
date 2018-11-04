5fe93508a1b65e382640be576e5c95771971c541
Kyrillos Walid Radi <30301905+KyrillosWalid@users.noreply.github.com>
2018-11-04 18:42:16 +0200

    Update S1AP.java

--- A. Theatre Square/S1AP.java
+++ A. Theatre Square/S1AP.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class S1AP {
     public static void main(String[] args) {
-        /*
-        **********************************************************
-        * THIS CODE BY KOKADEV...KYRILLOSWALIDRADI               *
-        * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
-        * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
-        * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
-        * WWW.FACEBOOK.COM/KOKADEV                               *
-        * WWW.GITHUB.COM/KOKADev                                 *
-        **********************************************************
-         */
+    /*
+   **********************************************************
+   * THIS CODE BY KYRILLOSWALIDRADI                         *
+   * ALL COPYRIGHYS ARE SAVED TO KYRILLOSWALIDRADI          *
+   * + 2 0 1 0 6 2 7 0 4 4 1 3 - + 2 0 1 2 1 1 2 8 0 3 5 8  *
+   * KYRILLOSWALIDRADI@HOTMAIL.COM                          *
+   * WWW.FACEBOOK.COM/KyrillosWalid                         *
+   * WWW.GITHUB.COM/KyrillosWalid                           *
+   **********************************************************
+   */
         Scanner input = new Scanner(System.in);
         long m = input.nextLong();
         long n = input.nextLong();
